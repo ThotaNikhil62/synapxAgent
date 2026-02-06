d2f059f9acca706736acb243e87c9678d4a68475
ThotaNikhil62 <21a31a0462@pragati.ac.in>
2026-02-06 16:50:39 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Synapx Claims Processing Agent");
+
+        String sampleText =
+                "POLICY NUMBER: P12345\n" +
+                "DATE OF LOSS: 12/05/2024\n" +
+                "LOCATION OF LOSS: New York\n" +
+                "ESTIMATE AMOUNT: 12000\n" +
+                "DESCRIPTION: Minor accident damage";
+
+        Map<String, String> fields = FieldExtractor.extract(sampleText);
+
+        ClaimResult result = Router.route(fields, sampleText);
+
+        System.out.println(result.toJson());
     }
 }
+
